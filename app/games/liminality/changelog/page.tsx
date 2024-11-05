47df68aa62bac96ca3624fe2bfbfcf3c43b6f86c
Lory <briciola.mc@gmail.com>
2024-11-05 19:43:40 +0100

    why the fuck isnt this working

--- app/games/liminality/changelog/page.tsx
+++ app/games/liminality/changelog/page.tsx
@@ -66,6 +66,7 @@ export default function CyberOdysseyChangelog() {
         </section>
       </main>
 
+
       <footer className="py-12 border-t border-white/10">
         <div className="container mx-auto px-4">
           <div className="flex flex-col md:flex-row justify-between items-center">
